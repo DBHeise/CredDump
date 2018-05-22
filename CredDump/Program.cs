6f7f3329bc88e85ede3ace648415eab5d32d4d24
DBHeise <david@heiseink.com>
2018-05-22 10:20:10 -0400

    Initial Commit

---
+++ CredDump/Program.cs
@@ -0,0 +1,67 @@
+﻿
+
+namespace CredDump
+{
+    using System;
+    using System.IO;
+    using System.Runtime.Serialization.Json;
+    using System.Text;
+    using Kraken.Security.Credentials;
+
+    [Serializable]
+    struct PwdData
+    {
+        public String Target;
+        public String UserName;
+        public String Password;
+    }
+
+    class Program
+    {
+
+        static DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(PwdData));
+
+        static String ConvertToString(PwdData data)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                serializer.WriteObject(ms, data);
+                return Encoding.Default.GetString(ms.ToArray());
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            
+            CredUtils.Credential[] creds;
+            CredUtils.CredEnum("*", out creds);
+            foreach(var cred in creds)
+            {
+                CredUtils.Credential c;
+                int rv = CredUtils.CredRead(cred.TargetName, cred.Type, out c);
+                
+                if (rv == 0)
+                {
+                    var target = c.TargetName.Split('=')[1];
+                    var pwd = new PwdData();
+                    pwd.Target = target;
+                    pwd.UserName = c.UserName;
+                    pwd.Password = c.CredentialBlob;
+                                        
+                    Console.WriteLine(ConvertToString(pwd));
+                }
+
+                //CredentialManagement.Credential c = new CredentialManagement.Credential();
+                //c.Target = cred.TargetName.Split('=')[1];
+                //if (c.Load())
+                //{
+                //    
+                //}
+
+            }
+
+        }
+
+    }
+}
